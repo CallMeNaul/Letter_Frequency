51fd7696fd8194b35b9330175e3b96e9899906d1
ADMIN@DESKTOP-RO1B9H5 <ADMIN@DESKTOP-RO1B9H5>
2023-09-28 15:51:04 +0700

    First Commit

--- Letter_Frequency.cpp
+++ Letter_Frequency.cpp
@@ -1,20 +1,38 @@
-// Letter_Frequency.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+﻿#include <iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Đọc file từ tập tin
+	FILE* f;
+	string s;
+	char c;
+
+	fopen_s(&f, "Mono-alphabetic ciphertext.txt", "r");
+	while ((c = fgetc(f)) != EOF) {
+		s += c;
+	}
+	fclose(f);
+	cout << "Cipher Text:\n\n-------------------------------------------\n" << s;
+	cout << "\n-------------------------------------------\n";
+
+	//Tính tần suất của chữ cái
+	int letFre[26], temp, letCount = 0;
+	for (int i = 0; i < 26; i++)
+	{
+		letFre[i] = 0;
+	}
+	for (int i = 0; i < s.length(); i++)
+	{
+		if ((int)s[i] >= 97 && (int)s[i] <= 122)
+		{
+			letCount++;
+			temp = ((int)s[i]) - 97;
+			letFre[temp]++;
+		}
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		cout << char(i + 97) << ": " << (double) letFre[i] *100 / letCount<<"%" << endl;
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
